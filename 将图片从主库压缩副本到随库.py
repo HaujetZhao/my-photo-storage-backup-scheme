87b0f61f63042d1e357e7116814eb559d63927d0
Haujet <1292756898@qq.com>
2021-10-19 10:58:00 +0800

    完善提示

--- 将图片从主库压缩副本到随库.py
+++ 将图片从主库压缩副本到随库.py
@@ -25,6 +25,7 @@ import glob
 import json
 import shlex
 import time
+from os import path
 from io import TextIOWrapper
 from pathlib import Path
 from pprint import pprint
@@ -57,6 +58,16 @@ from pprint import pprint
 # 在随库中用于存放完好视频的记录文件
 完好视频记录文件 = '.intact_video'
 
+# 是否要删除随库中的冗余的照片、视频？
+# 解释一下：随库中的图片是主库中的压缩版本
+#    但有时，我们会整理主库中的图片，例如删除、移动、重命名
+#    这时，如果只将新图片增量压缩到随库
+#    随库中就会有以往的旧照片、视频，不干净，需要手动再去清理随库
+#    如果启用删除，则会比较随库相比主库，有哪些多余的照片、视频，然后删除
+#    但是，这会导致你在随库中新创建的图片、视频也被删掉
+#    所以，请辩证性考虑要不要删除
+删除随库冗余媒体 = True
+
 
 def main(): 
     
@@ -65,13 +76,20 @@ def main():
     忽略文件夹列表 = [y 
                 for x in 忽略文件夹.strip().splitlines() 
                 for y in glob.glob(f'{主库位置}/{x}')
-                if os.path.isdir(y)]
+                if path.isdir(y)]
     # print('忽略以下文件夹：')
     # pprint(忽略文件夹列表)
     合法图片后缀 = [x for x in 图片后缀.strip().splitlines()]
     合法视频后缀 = [x for x in 视频后缀.strip().splitlines()]
     
     # ========================================================================
+    print(f'\n主库位置：{主库位置}')
+    print(f'随库位置：{随库位置}\n')
+    
+    if not path.exists(主库位置) or not path.exists(随库位置):
+        input('错误：“主库”或“随库”位置不存在\n请先编辑此脚本，指派正确的“主库”、“随库”位置，再运行脚本\n\n按下回车结束')
+        return False
+    
     print('正在统计文件中……\n')
     遍历文件开始时间 = time.time()
     # 获得主库中所有文件的相对位置，组成列表，用于比较
@@ -88,9 +106,9 @@ def main():
 
     主库图片列表, 主库视频列表, 主库其它文件列表 = [], [], []
     for file in 主库文件列表: 
-        if os.path.splitext(file[1])[1].lower() in 合法图片后缀:
+        if path.splitext(file[1])[1].lower() in 合法图片后缀:
             主库图片列表.append(file)
-        elif os.path.splitext(file[1])[1].lower() in 合法视频后缀:
+        elif path.splitext(file[1])[1].lower() in 合法视频后缀:
             主库视频列表.append(file)
         else:
             主库其它文件列表.append(file)
@@ -109,9 +127,9 @@ def main():
 
     随库图片列表, 随库视频列表, 随库其它文件列表 = [], [], []
     for file in 随库文件列表:
-        if os.path.splitext(file[1])[1].lower() in 合法图片后缀:
+        if path.splitext(file[1])[1].lower() in 合法图片后缀:
             随库图片列表.append(file)
-        elif os.path.splitext(file[1])[1].lower() in 合法视频后缀:
+        elif path.splitext(file[1])[1].lower() in 合法视频后缀:
             随库视频列表.append(file)
         else:
             随库其它文件列表.append(file)
@@ -121,10 +139,10 @@ def main():
     统计文件消耗时间 = time.time() - 遍历文件开始时间
     # ========================================================================
     # 去除已压缩的图片，得到主库中都有哪些图片需要压缩副本到随库
-    随库图片前缀列表 = [os.path.splitext(f'{x[0]}/{x[1]}'.lstrip('/'))[0]
+    随库图片前缀列表 = [path.splitext(f'{x[0]}/{x[1]}'.lstrip('/'))[0]
                 for x in 随库图片列表]
     主库需要压缩的图片列表 = [y for y in 主库图片列表  
-                   if os.path.splitext(f'{y[0]}/{y[1]}'.lstrip('/'))[0] 
+                   if path.splitext(f'{y[0]}/{y[1]}'.lstrip('/'))[0] 
                    not in 随库图片前缀列表]
     
     # 去除已压缩的视频，得到主库中都有哪些视频需要压缩副本到随库
@@ -132,26 +150,31 @@ def main():
     # 因此，需要对随库中每一个文件检验一下，看是否为完整的视频文件
     检测视频开始时间 = time.time()
     随库完好视频列表 = 筛选出完好的视频列表(随库位置, 随库视频列表, 完好视频记录文件)
-    随库视频前缀列表 = [os.path.splitext(x)[0] 
+    随库视频前缀列表 = [path.splitext(x)[0] 
                 for x in 随库完好视频列表]
     主库需要压缩的视频列表 = [x for x in 主库视频列表
-                   if os.path.splitext(f'{x[0]}/{x[1]}'.lstrip('/'))[0] 
+                   if path.splitext(path.join(x[0], x[1]))[0] 
                    not in 随库视频前缀列表]
+    # print(f'随库完好视频列表:{随库完好视频列表}')
+    # print(f'随库视频前缀列表：{随库视频前缀列表}')
+    # print(f'主库需要压缩的视频列表：{主库需要压缩的视频列表}')
+    # print(f'：{}')
+    # input('...')
     
     检测视频消耗时间 = time.time() - 检测视频开始时间
     # ========================================================================
     # 当主库中删除视频图片后，肯定也希望这个希望能同步到随库
     # 所以要检测一下随库中多了哪些文件，将其删除
-    主库图片前缀列表 = [os.path.splitext(
+    主库图片前缀列表 = [path.splitext(
         f'{x[0]}/{x[1]}'.lstrip('/'))[0] for x in 主库图片列表]
     随库需要删除图片列表 = [x for x in 随库图片列表 
-                  if os.path.splitext(f'{x[0]}/{x[1]}'.lstrip('/'))[0]
+                  if path.splitext(f'{x[0]}/{x[1]}'.lstrip('/'))[0]
                   not in 主库图片前缀列表]
     
-    主库视频前缀列表 = [os.path.splitext(
+    主库视频前缀列表 = [path.splitext(
         f'{x[0]}/{x[1]}'.lstrip('/'))[0] for x in 主库视频列表]
     随库需要删除视频列表 = [x for x in 随库视频列表
-                  if os.path.splitext(f'{x[0]}/{x[1]}'.lstrip('/'))[0]
+                  if path.splitext(f'{x[0]}/{x[1]}'.lstrip('/'))[0]
                   not in 主库视频前缀列表]
     # ========================================================================
 
@@ -163,8 +186,8 @@ def main():
     print(f'    有 {len(主库其它文件列表)} 个其它文件\n')
 
     print(f'随库中目前共有 {len(随库文件列表)} 个文件')
-    print(f'    有 {len(随库图片列表)} 个图片，其中有 {len(随库需要删除图片列表)} 个需要删除')
-    print(f'    有 {len(随库视频列表)} 个视频，其中有 {len(随库需要删除视频列表)} 个需要删除')
+    print(f'    有 {len(随库图片列表)} 个图片，其中有 {len(随库需要删除图片列表)} 个冗余图片')
+    print(f'    有 {len(随库视频列表)} 个视频，其中有 {len(随库需要删除视频列表)} 个冗余视频')
     print(f'    有 {len(随库其它文件列表)} 个其它文件\n')
     
     print(f'遍历文件耗时 {"%5.2f" % 统计文件消耗时间}s，检测视频完整性耗时 {"%5.2f" % 检测视频消耗时间}s\n')
@@ -173,8 +196,15 @@ def main():
     压缩主库图片到随库(主库需要压缩的图片列表)
     压缩主库视频到随库(主库需要压缩的视频列表)
     
-    依列表删除随库文件(随库需要删除图片列表)
-    依列表删除随库文件(随库需要删除视频列表)
+    if 删除随库冗余媒体:
+        if 随库需要删除图片列表 and 随库需要删除视频列表:
+            print(f'正在删除随库中的冗余媒体文件……\n')
+            依列表删除随库文件(随库需要删除图片列表)
+            依列表删除随库文件(随库需要删除视频列表)
+        else:
+            print(f'随库中没有需要删除的冗余媒体文件\n')
+    else:
+        print(f'不删除随库中的冗余媒体文件\n')
     
     
     
@@ -210,8 +240,8 @@ def 判断视频是否已压制完成(压缩视频路径):
 
 def 筛选出完好的视频列表(根目录, 列表, 记录文件):
     # 列表中包含的项是这样的： ['relative/path', 'file.name']
-    记录文件 = os.path.join(根目录, 记录文件)
-    if not os.path.exists(记录文件):
+    记录文件 = path.join(根目录, 记录文件)
+    if not path.exists(记录文件):
         print(f'创建视频完整性记录文件：{记录文件}')
         with open(记录文件, 'w'):
             ...
@@ -219,13 +249,13 @@ def 筛选出完好的视频列表(根目录, 列表, 记录文件):
     # 得到还没有记录的视频有哪些
     with open(记录文件, encoding='utf-8') as f:
         记录列表 = [x.strip() for x in f.readlines()]
-        待测列表 = [x for x in 列表 if os.path.join(x[0], x[1]) not in 记录列表]
+        待测列表 = [x for x in 列表 if path.join(x[0], x[1]) not in 记录列表]
     
     # 对没有记录的视频进行检测
     with open(记录文件, 'a', encoding='utf-8') as f:
         for 待测项 in 待测列表:
-            相对路径 = os.path.join(待测项[0], 待测项[1])
-            if 判断视频是否已压制完成(os.path.join(根目录, 相对路径)):
+            相对路径 = path.join(待测项[0], 待测项[1])
+            if 判断视频是否已压制完成(path.join(根目录, 相对路径)):
                 记录列表.append(相对路径)
                 f.write(相对路径 + '\n')
     
@@ -236,7 +266,7 @@ def 筛选出完好的视频列表(根目录, 列表, 记录文件):
 def 依列表删除随库文件(文件列表):
     # 列表中包含的项是这样的： ['relative/path', 'file.name']
     for item in 文件列表:
-        完整路径 = os.path.join(随库位置, item[0], item[1])
+        完整路径 = path.join(随库位置, item[0], item[1])
         try:
             os.remove(完整路径)
         except Exception as e:
@@ -247,15 +277,15 @@ def 压缩主库图片到随库(图片列表):
     # 列表中包含的项是这样的： ['relative/path', 'file.name']
     print(f'开始压缩图片到随库，总共有 {len(图片列表)} 个图片需要压缩\n')
     for index, item in enumerate(图片列表):
-        src = os.path.join(主库位置, item[0], item[1])
-        src_rel = os.path.join(item[0], item[1])
-        dst = os.path.join(随库位置, item[0], item[1])
-        dst = os.path.splitext(dst)[0] + '.webp'  # 将输出文件格式改为 webp
-        dst_rel = os.path.splitext(src_rel)[0] + '.webp'
+        src = path.join(主库位置, item[0], item[1])
+        src_rel = path.join(item[0], item[1])
+        dst = path.join(随库位置, item[0], item[1])
+        dst = path.splitext(dst)[0] + '.webp'  # 将输出文件格式改为 webp
+        dst_rel = path.splitext(src_rel)[0] + '.webp'
         
         # 确保目标文件存在
-        if not os.path.exists(os.path.dirname(dst)):
-            os.makedirs(os.path.dirname(dst))
+        if not path.exists(path.dirname(dst)):
+            os.makedirs(path.dirname(dst))
         
         宽高比 = subprocess.run(shlex.split(
             f'magick identify -format "%[fx:w/h]" "{src}"'),
@@ -273,7 +303,7 @@ def 压缩主库图片到随库(图片列表):
                 shlex.split(
                     # 将图像设为 300w 像素大小，只对大图缩小，不对小图放大
                     # 质量 70
-                    f'magick "{src}" -resize "5000000@>" -quality 70 "{dst}"'
+                    f'magick "{src}" -resize "3000000@>" -quality 70 "{dst}"'
                 ), capture_output=True
             )
         else:
@@ -287,7 +317,7 @@ def 压缩主库图片到随库(图片列表):
             )
         print(f'        新文件路径 {dst_rel}')
         print(f'        压缩后大小 {文件大小(dst)}')
-        压缩比例 = os.path.getsize(dst) / os.path.getsize(src) * 100
+        压缩比例 = path.getsize(dst) / path.getsize(src) * 100
         print(f'        比原来减小 {"%0.2f" % (100 - 压缩比例)}%\n')
     
     
@@ -297,14 +327,14 @@ def 压缩主库视频到随库(视频列表):
     # 列表中包含的项是这样的： ['relative/path', 'file.name']
     print(f'开始压缩视频到随库，总共有 {len(视频列表)} 个视频需要压缩\n')
     for index, item in enumerate(视频列表):
-        src = os.path.join(主库位置, item[0], item[1])
-        src_rel = os.path.join(item[0], item[1])
-        dst = os.path.join(随库位置, item[0], item[1])
+        src = path.join(主库位置, item[0], item[1])
+        src_rel = path.join(item[0], item[1])
+        dst = path.join(随库位置, item[0], item[1])
         dst_rel = src_rel
         
         # 确保目标文件存在
-        if not os.path.exists(os.path.dirname(dst)):
-            os.makedirs(os.path.dirname(dst))
+        if not path.exists(path.dirname(dst)):
+            os.makedirs(path.dirname(dst))
         
         print(f'    正在压缩第 {index+1} 个视频（共 {len(视频列表)} 个）：')
         print(f'        原视频路径 {src_rel}')
@@ -339,9 +369,9 @@ def 压缩主库视频到随库(视频列表):
         print(f'        新分辨率   {新分辨率}')
         
         命令 = f'ffmpeg -hide_banner -y -i "{src}" -vf "scale={新分辨率}" -crf 23 "{dst}"'
-        process = subprocess.Popen(shlex.split(命令), shell=True,
-                               stdout=subprocess.PIPE, 
-                               stderr=subprocess.STDOUT)
+        process = subprocess.Popen(shlex.split(命令), 
+                                stdout=subprocess.PIPE, 
+                                stderr=subprocess.STDOUT)
         for line in TextIOWrapper(process.stdout, encoding='utf-8', newline='\r'):
             if line.startswith('frame='):
                 print(f"\r{line}", end='')
@@ -353,11 +383,11 @@ def 压缩主库视频到随库(视频列表):
         # print(f'        新文件路径 {dst_rel}')
         print(f'        新比特率   {"%0.2f" % 新视频比特率}Mbps')
         print(f'        压缩后大小 {文件大小(dst)}')
-        压缩比例 = os.path.getsize(dst) / os.path.getsize(src) * 100
+        压缩比例 = path.getsize(dst) / path.getsize(src) * 100
         print(f'        比原来减小 {"%0.2f" % (100 - 压缩比例)}%\n')
         
         # 既然这个视频压缩完了，就把它记录下来，下次就不用检查这个视频了
-        with open(os.path.join(随库位置, 完好视频记录文件), 'a', encoding='utf-8') as f:
+        with open(path.join(随库位置, 完好视频记录文件), 'a', encoding='utf-8') as f:
             f.write(f'{dst_rel}\n')
             
 
@@ -375,9 +405,9 @@ def 适当大小(size):
     return (f'{"%0.2f" % size}{进位单位[进位]}')
 
 def 文件大小(文件路径):
-    if not os.path.exists(文件路径):
+    if not path.exists(文件路径):
         return False
-    return 适当大小(os.path.getsize(文件路径))
+    return 适当大小(path.getsize(文件路径))
 
 
 
